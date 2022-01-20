5ef7957baceb2dc104a1d1360f94135cd1920b8d
harshperson <harshperson@gmail.com>
2022-01-20 19:46:24 +0200

    bubble sort

---
+++ index.js
@@ -0,0 +1,18 @@
+var arr = [30,2,5,14,50,55,1,30,56,55,545,77,3,30,2,5,14,50,55,1,30,56,55,545,77,3];
+console.log(arr);
+bubl=(arr)=>{
+  
+  for (let i=0; i < arr.length-1 ; i++) {
+    for (let j = 0; j < arr.length-1; j++) {
+      let swallow=0;
+      if(arr[j]>arr[j+1]){
+        swallow=arr[j];
+        arr[j]=arr[j+1];
+        arr[j+1]=swallow;
+      }
+    }
+  }
+  
+}
+bubl(arr)
+console.log(arr);
